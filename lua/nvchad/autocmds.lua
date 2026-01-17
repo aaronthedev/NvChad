afdfb7aae3dcce565b22693ba96d0285fb7cb1df
siduck <siduck@tutanota.com>
2026-01-17 22:14:41 +0530

    fix: handle opts() in TSInstallAll CMD

--- lua/nvchad/autocmds.lua
+++ lua/nvchad/autocmds.lua
@@ -37,5 +37,6 @@ local create_cmd = vim.api.nvim_create_user_command
 
 create_cmd("TSInstallAll", function()
   local spec = require("lazy.core.config").plugins["nvim-treesitter"]
-  require("nvim-treesitter").install(spec.opts.ensure_installed)
+  local opts = type(spec.opts) == "table" and spec.opts or {}
+  require("nvim-treesitter").install(opts.ensure_installed)
 end, {})
