382a8bd7660fe7bf15b5405d80d7f02bc6d1ea9b
aaronthedev <aaron@AaronDesk.localdomain>
2026-01-24 07:57:29 +0700

    Unreal

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -3,8 +3,16 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 
-lspconfig.clangd.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { "clangd", "--background-index", "--clang-tidy" },
-})
+lspconfig.clangd.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=never",
+    "--completion-style=detailed",
+        -- 🔑 THIS IS THE IMPORTANT PART
+    "--query-driver=*",
+  },
+})
