9471bca9976d10d977cb6bb99ac6f6941c56f75f
aaronthedev <aaronthedev>
2026-05-19 20:58:44 +0800

    adf

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  keys = {
+    { "<leader>t", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
+  },
+  opts = {
+    size = 20,
+    direction = "float",
+  },
+}
