d0b0a00c0d216da1eda419caa8a8425f28d5c189
aaronthedev <iengtheansovann@gmail.com>
2026-07-30 23:49:14 +0700

    works, but :UBT gencompiledb doesnt, gen manually works

--- lua/plugins/unrealdev.lua
+++ lua/plugins/unrealdev.lua
@@ -33,25 +33,37 @@ return {
       "j-hui/fidget.nvim",
       {
         "romus204/tree-sitter-manager.nvim",
+
         opts = {
-          ensure_installed = { "cpp", "ushader", "verse" },
-          highlight = { "cpp", "ushader", "verse" },
+          ensure_installed = {
+            "cpp",
+            "ushader",
+            "verse",
+          },
+
+          highlight = {
+            "cpp",
+            "ushader",
+            "verse",
+          },
+
           border = "rounded",
+
           languages = {
             cpp = {
               install_info = {
-                url = "https://github.com/taku25/tree-sitter-unreal-cpp",
-                revision = "e2b94d3bd3ce359ff732116cc21f34ecbecfca50",
+                url = "https://github.com/taku25/tree-sitter-cpp",
                 use_repo_queries = true,
               },
-              requires = { "c" },
             },
+
             ushader = {
               install_info = {
                 url = "https://github.com/taku25/tree-sitter-unreal-shader",
                 use_repo_queries = true,
               },
             },
+
             verse = {
               install_info = {
                 url = "https://github.com/taku25/tree-sitter-verse",
@@ -60,49 +72,76 @@ return {
             },
           },
         },
-        config = function(_, opts)
 
+        config = function(_, opts)
           ---------------------------------------------------------------------------
           -- Neovim 0.11 compatibility shim for tree-sitter-manager.nvim
           ---------------------------------------------------------------------------
 
           vim.list = vim.list or {}
 
-          vim.list.unique = vim.list.unique or function(items)
-            local result = {}
-            local seen = {}
+          if not vim.list.unique then
+            vim.list.unique = function(items)
+              local result = {}
+              local seen = {}
 
-            for _, item in ipairs(items) do
-              if not seen[item] then
-                seen[item] = true
-                result[#result + 1] = item
+              for _, item in ipairs(items) do
+                if not seen[item] then
+                  seen[item] = true
+                  result[#result + 1] = item
+                end
               end
-            end
 
-            return result
+              return result
+            end
           end
 
-          vim.filetype.add({
+          ---------------------------------------------------------------------------
+          -- Unreal-related file types
+          ---------------------------------------------------------------------------
+
+          vim.filetype.add {
             extension = {
               verse = "verse",
               usf = "ushader",
               ush = "ushader",
             },
-          })
+          }
+
+          ---------------------------------------------------------------------------
+          -- Tree-sitter manager
+          ---------------------------------------------------------------------------
+
           require("tree-sitter-manager").setup(opts)
-          local group = vim.api.nvim_create_augroup("MyTreesitter", { clear = true })
+
+          ---------------------------------------------------------------------------
+          -- Enable Tree-sitter highlighting
+          ---------------------------------------------------------------------------
+
+          local group = vim.api.nvim_create_augroup("UnrealTreesitter", { clear = true })
+
           vim.api.nvim_create_autocmd("FileType", {
             group = group,
             pattern = opts.highlight,
+
             callback = function(args)
-              vim.treesitter.start(args.buf)
+              local ok, err = pcall(vim.treesitter.start, args.buf)
+
+              if not ok then
+                vim.schedule(function()
+                  vim.notify(
+                    ("Failed to start Tree-sitter for '%s': %s"):format(vim.bo[args.buf].filetype, tostring(err)),
+                    vim.log.levels.WARN
+                  )
+                end)
+              end
             end,
           })
         end,
       },
     },
     config = function()
-      require("UnrealDev").setup({
+      require("UnrealDev").setup {
         setup_modules = {
           UBT = true,
           UEP = true,
@@ -112,7 +151,7 @@ return {
           UEA = true,
           UNX = true,
         },
-      })
+      }
     end,
   },
 }
