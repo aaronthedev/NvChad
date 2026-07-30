0e8b81bbc622565cc32e7b6a131eef402ab466c6
aaronthedev <iengtheansovann@gmail.com>
2026-07-31 01:25:11 +0700

    key

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,16 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--map("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to Implementation" })
+map("n", "gr", vim.lsp.buf.references, { desc = "Find References" })
+
+map("n", "gd", function()
+  local api = require("UnrealDev.api")
+  if api.goto_definition then
+    api.goto_definition({ has_bang = false })
+  else
+    vim.lsp.buf.definition()
+  end
+end, { desc = "UE Go to Definition" })
+
