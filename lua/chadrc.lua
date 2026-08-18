6b8925ecd9d70b994a6ff54e16f63e10b51ae8bb
aaronthedev <iengtheansovann@gmail.com>
2026-08-18 16:18:01 +0700

    color

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -12,6 +12,31 @@ M.base46 = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
+	hl_override = {
+		Normal = { bg = "#000000", fg = "#909090" },
+		NormalNC = { bg = "#000000", fg = "#909090" },
+
+		-- Floating windows
+		NormalFloat = { bg = "#000000" },
+		FloatBorder = { bg = "#000000" },
+
+		-- Sign / number columns
+		SignColumn = { bg = "#000000" },
+		LineNr = { bg = "#000000" },
+		CursorLineNr = { bg = "#000000" },
+
+		-- Cursor line
+		CursorLine = { bg = "#080808" },
+
+		-- End-of-buffer "~"
+		EndOfBuffer = { bg = "#000000" },
+
+		-- NvimTree
+		NvimTreeNormal = { bg = "#000000" },
+		NvimTreeNormalNC = { bg = "#000000" },
+		NvimTreeEndOfBuffer = { bg = "#000000" },
+		NvimTreeWinSeparator = { bg = "#000000" }
+	}
 }
 
 -- M.nvdash = { load_on_startup = true }
--- lua/options.lua
+++ lua/options.lua
@@ -9,3 +9,8 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.writebackup = false
 vim.opt.undofile = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = false
